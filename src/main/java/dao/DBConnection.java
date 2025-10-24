45b341b76d28b93e93483c8f896eb946e5e203e8
NITHINradarapu <radarapunithin05@gmail.com>
2025-10-24 15:13:52 +0530

    This an Online Quiz Management Portal developed using JFS

---
+++ src/main/java/dao/DBConnection.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/quizdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "head";
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizdb", "root", "head");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
